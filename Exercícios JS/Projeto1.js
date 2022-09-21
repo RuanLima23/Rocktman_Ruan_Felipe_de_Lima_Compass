700b99cade6f58d484438467076df88e2df70d89
RuanLima <delimaruanf@gmail.com>
2022-09-21 19:55:53 -0300

    Projetos

---
+++ Exercícios JS/Projeto1.js
@@ -0,0 +1,30 @@
+// Invertendo as cores
+
+let cor1 = 'Vermelho';
+let cor2 = 'Azul';
+
+let cor3 = cor1;
+
+function inverteCores(){
+    cor1 = cor2,
+    cor2 = cor3
+};
+
+inverteCores()
+console.log(cor1);
+console.log(cor2);
+
+
+
+// let cor1 = 'Vermelho';
+// let cor2 = 'Azul';
+
+// function inverteCores(){
+//     cor1 = cor2,
+//     cor2 = cor1
+// };
+
+// inverteCores();
+
+// console.log(cor1);
+// console.log(cor2);
